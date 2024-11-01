964eb40ce4cb6573e2d851bcda31ab69bc07f611
Tarik Zegmott <tzegmott@gmail.com>
2024-11-01 10:14:01 -0400

    lazyvim: update

--- dot_config/LazyVim/lua/plugins/db.lua
+++ dot_config/LazyVim/lua/plugins/db.lua
@@ -1,15 +1,39 @@
 return {
-  "kndndrj/nvim-dbee",
-  dependencies = {
-    "MunifTanjim/nui.nvim",
+  {
+    "kndndrj/nvim-dbee",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    cmd = {
+      "Dbee",
+    },
+    build = function()
+      -- Install tries to automatically detect the install method.
+      -- if it fails, try calling it with one of these parameters:
+      --    "curl", "wget", "bitsadmin", "go"
+      require("dbee").install()
+    end,
+    config = function()
+      require("dbee").setup(--[[optional config]])
+    end,
   },
-  build = function()
-    -- Install tries to automatically detect the install method.
-    -- if it fails, try calling it with one of these parameters:
-    --    "curl", "wget", "bitsadmin", "go"
-    require("dbee").install()
-  end,
-  config = function()
-    require("dbee").setup(--[[optional config]])
-  end,
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      { "tpope/vim-dadbod", lazy = true },
+      { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+    },
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
+    },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      -- Auto-execute query
+      vim.g.db_ui_auto_execute_table_helpers = 1
+    end,
+  },
 }
