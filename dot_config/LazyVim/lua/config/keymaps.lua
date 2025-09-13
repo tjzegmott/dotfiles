e255dbc67f35941df4c91bf98459e3d64808c977
Tarik Zegmott <tzegmott@gmail.com>
2025-09-12 22:48:21 -0400

    Lazyvim+vit: custom nvim taskwarrior scripts

--- dot_config/LazyVim/lua/config/keymaps.lua
+++ dot_config/LazyVim/lua/config/keymaps.lua
@@ -1,3 +1,24 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Taskwarrior keymaps
+vim.keymap.set("n", "<leader>T", "", { desc = "+taskwarrior" })
+vim.keymap.set(
+	"n",
+	"<leader>Tg",
+	"<Cmd>lua require('user_functions.tasks').go_to_task_in_taskwarrior_tui()<CR>",
+	{ desc = "Go to task", noremap = true, silent = true }
+)
+vim.keymap.set(
+	"n",
+	"<leader>Td",
+	"<Cmd>lua require('user_functions.tasks').mark_task_done()<CR>",
+	{ desc = "Mark task as done", noremap = true, silent = true }
+)
+vim.keymap.set(
+	"n",
+	"<leader>Tc",
+	"<Cmd>lua require('user_functions.tasks').create_or_update_task()<CR>",
+	{ desc = "Create or update task", noremap = true, silent = true }
+)
---
+++ dot_config/taskwarrior-tui/shortcut-scripts/executable_switch_to_tui.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# Capture the current session name
+current_session=$(tmux display-message -p '#S')
+
+# Parse the task description from the argument
+TASK_DESCRIPTION="$1"
+
+# Create a new tmux session named 'new-task-session' and run 'tui -r current'
+tmux new-session -d -s new-task-session "vit"
+
+# Sleep for a bit to allow tui to load
+sleep 0.5
+
+# Send the keystrokes needed to filter tasks by description to the target pane
+tmux send-keys -t new-task-session:1.1 "/$TASK_DESCRIPTION" C-m
+
+# Attach to the new session
+tmux switch-client -t new-task-session
+
+# Wait for the session to be closed, either by the user or some other way
+while tmux has-session -t new-task-session 2>/dev/null; do
+  sleep 0.1
+done
+
+# Switch back to the original session
+tmux switch-client -t $current_session
