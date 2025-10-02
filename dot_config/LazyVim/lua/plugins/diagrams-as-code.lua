d7038c7e4cd86cbdf3e3def9fec6c13300f9480e
Tarik Zegmott <tzegmott@gmail.com>
2025-10-02 13:05:14 -0400

    lazyvim: add plugin and user function

---
+++ dot_config/LazyVim/lua/plugins/diagrams-as-code.lua
@@ -0,0 +1,10 @@
+return {
+  "terrastruct/d2-vim",
+  ft = { "d2" },
+  keys = {
+    { "<leader>d2", false },
+    { "<leader>d2", mode = "v", false },
+    { "<leader>rd2", false },
+    { "<leader>rd2", mode = "v", false },
+  },
+}
